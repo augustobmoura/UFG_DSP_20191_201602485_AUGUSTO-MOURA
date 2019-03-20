b2263bf5fab8aafb2a6e498c964f2a1681d803bc
augustobmoura <augusto.borgesm@gmail.com>
2019-03-20 19:32:10 -0300

    First commit

---
+++ src/main/java/br/com/augustobmoura/dsp20191/App.java
@@ -0,0 +1,13 @@
+package br.com.augustobmoura.dsp20191;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/br/com/augustobmoura/dsp20191/AppTest.java
@@ -0,0 +1,20 @@
+package br.com.augustobmoura.dsp20191;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
